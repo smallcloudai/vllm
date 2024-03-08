42780151d90d48403c5d4eebe31339b60a18d6e4
JegernOUTT <sergey.vakhreev@gmail.com>
2024-03-08 18:09:48 +1030

    starcoder2 lora support

--- vllm/model_executor/models/starcoder2.py
+++ vllm/model_executor/models/starcoder2.py
@@ -25,6 +25,7 @@ import torch
 from torch import nn
 from transformers import Starcoder2Config
 
+from vllm.config import LoRAConfig
 from vllm.attention import Attention
 from vllm.compilation.decorators import support_torch_compile
 from vllm.config import CacheConfig, VllmConfig
@@ -212,8 +213,10 @@ class Starcoder2Model(nn.Module):
         quant_config = vllm_config.quant_config
 
         self.config = config
+        self.padding_idx = config.pad_token_id
         self.vocab_size = config.vocab_size
 
+        # TODO: consider padding_idx (currently removed)
         self.embed_tokens = VocabParallelEmbedding(
             config.vocab_size,
             config.hidden_size,
@@ -258,6 +261,28 @@ class Starcoder2Model(nn.Module):
 
 
 class Starcoder2ForCausalLM(nn.Module, SupportsPP):
+    packed_modules_mapping = {
+        "qkv_proj": [
+            "q_proj",
+            "k_proj",
+            "v_proj",
+        ]
+    }
+
+    # LoRA specific attributes
+    supported_lora_modules = [
+        "qkv_proj",
+        "o_proj",
+        "gate_up_proj",
+        "down_proj",
+        "embed_tokens",
+        "lm_head",
+    ]
+    embedding_modules = {
+        "embed_tokens": "input_embeddings",
+        "lm_head": "output_embeddings",
+    }
+    embedding_padding_modules = ["lm_head"]
 
     def __init__(self, *, vllm_config: VllmConfig, prefix: str = ""):
         super().__init__()
@@ -270,13 +295,18 @@ class Starcoder2ForCausalLM(nn.Module, SupportsPP):
         self.unpadded_vocab_size = config.vocab_size
         if config.tie_word_embeddings:
             self.lm_head = self.model.embed_tokens
+        if lora_config:
+            self.unpadded_vocab_size += lora_config.lora_extra_vocab_size
         else:
             self.unpadded_vocab_size = config.vocab_size
             self.lm_head = ParallelLMHead(
                 self.unpadded_vocab_size,
                 config.hidden_size,
                 org_num_embeddings=config.vocab_size,
-                padding_size=DEFAULT_VOCAB_PADDING_SIZE,
+                padding_size=DEFAULT_VOCAB_PADDING_SIZE
+                # We need bigger padding if using lora for kernel
+                # compatibility
+                if not lora_config else lora_config.lora_vocab_padding_size,
                 quant_config=quant_config,
                 prefix=f"{prefix}.lm_head",
             )
